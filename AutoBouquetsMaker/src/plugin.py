dd2152c8978e0ee566ba47d287e6ef7eba79b80d
Orlandoxx <95180049+Orlandoxx@users.noreply.github.com>
2024-08-04 01:54:54 +0300

    [plugin.py] Fix localisation of descriptions (#303)

--- AutoBouquetsMaker/src/plugin.py
+++ AutoBouquetsMaker/src/plugin.py
@@ -107,7 +107,7 @@ def Plugins(**kwargs):
 	plist = []
 	if any([nimmanager.hasNimType(x) for x in ("DVB-S", "DVB-T", "DVB-C")]):
 		plist.append(PluginDescriptor(name="AutoBouquetsMakerSessionStart", where=[PluginDescriptor.WHERE_AUTOSTART, PluginDescriptor.WHERE_SESSIONSTART], fnc=Scheduleautostart, wakeupfnc=AutoBouquetsMakerWakeupTime, needsRestart=True))
-		plist.append(PluginDescriptor(name=_("AutoBouquetsMaker"), description="Scan and create bouquets.", where=PluginDescriptor.WHERE_MENU, fnc=AutoBouquetsMakerSetup, needsRestart=True))
+		plist.append(PluginDescriptor(name=_("AutoBouquetsMaker"), description=_("Scan and create bouquets."), where=PluginDescriptor.WHERE_MENU, fnc=AutoBouquetsMakerSetup, needsRestart=True))
 		if config.autobouquetsmaker.extensions.getValue():
-			plist.append(PluginDescriptor(name=_("AutoBouquetsMaker Scanner"), description="Scan and create bouquets.", where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=startscan, needsRestart=True))
+			plist.append(PluginDescriptor(name=_("AutoBouquetsMaker Scanner"), description=_("Scan and create bouquets."), where=PluginDescriptor.WHERE_EXTENSIONSMENU, fnc=startscan, needsRestart=True))
 	return plist
